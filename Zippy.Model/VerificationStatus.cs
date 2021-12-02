f894962bed6dc32b5958face24ed098292ba1fdb
David <mathew@zippy.cash>
2021-12-02 15:13:53 -0500

    Create "Document", "VerificationStatuses" and "DocumentTypes" models

---
+++ Zippy.Model/VerificationStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zippy.Model
+{
+    /// <summary>
+    /// Entity represents Verification Status, Values are pending, accepted, or rejected
+    /// </summary>
+    public class VerificationStatus : ZippyBase
+    {
+        public VerificationStatus()
+        {
+            Documents = new List<Document>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Document> Documents { get; set; }
+    }
+}
