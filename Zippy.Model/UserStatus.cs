9ce853b5b0019ded8847850df656f1c492062af7
David <mathew@zippy.cash>
2021-12-03 08:19:49 -0500

    Create "Users", "UserStatuses", "UserRoles" and "Roles" models

---
+++ Zippy.Model/UserStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Zippy.Model
+{
+    /// <summary>
+    /// UserStatus entity, Statuses will be activated, deactivated, suspended, pending
+    /// </summary>
+    public class UserStatus : ZippyBase
+    {
+        public UserStatus()
+        {
+            Users = new List<User>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<User> Users { get; set; }
+    }
+}
