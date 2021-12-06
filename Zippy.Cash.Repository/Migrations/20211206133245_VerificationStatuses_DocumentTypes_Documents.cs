fd364f1186b833a1f67b7a326f6f42b40f75e793
David <mathew@zippy.cash>
2021-12-06 08:33:13 -0500

    Create "Document", "VerificationStatuses" and "DocumentTypes" models

---
+++ Zippy.Cash.Repository/Migrations/20211206133245_VerificationStatuses_DocumentTypes_Documents.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zippy.Cash.Repository.Migrations
+{
+    public partial class VerificationStatuses_DocumentTypes_Documents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DocumentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VerificationStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VerificationStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EntityId = table.Column<int>(type: "int", nullable: false),
+                    VerificationStatusId = table.Column<int>(type: "int", nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOnUTC = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documents_DocumentTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "DocumentTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Documents_VerificationStatuses_VerificationStatusId",
+                        column: x => x.VerificationStatusId,
+                        principalTable: "VerificationStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Account",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AccountId", "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { "f6432a08-9c77-43c2-9449-8f8d79962953", new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9526), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9526) });
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9778), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9779) });
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9781), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9782) });
+
+            migrationBuilder.UpdateData(
+                table: "Currency",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9858), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9858) });
+
+            migrationBuilder.UpdateData(
+                table: "Currency",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9860), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9860) });
+
+            migrationBuilder.UpdateData(
+                table: "Organization",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9921), new DateTime(2021, 12, 6, 13, 32, 45, 427, DateTimeKind.Utc).AddTicks(9922) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(28), new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(28) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(30), new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(30) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(32), new DateTime(2021, 12, 6, 13, 32, 45, 428, DateTimeKind.Utc).AddTicks(32) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_TypeId",
+                table: "Documents",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_VerificationStatusId",
+                table: "Documents",
+                column: "VerificationStatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "DocumentTypes");
+
+            migrationBuilder.DropTable(
+                name: "VerificationStatuses");
+
+            migrationBuilder.UpdateData(
+                table: "Account",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AccountId", "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { "cc9a468a-41e4-483d-b353-b83d04d8b07c", new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5331), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5332) });
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5643), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5643) });
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5645), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5646) });
+
+            migrationBuilder.UpdateData(
+                table: "Currency",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5725), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5725) });
+
+            migrationBuilder.UpdateData(
+                table: "Currency",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5726), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5727) });
+
+            migrationBuilder.UpdateData(
+                table: "Organization",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5798), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5798) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5961), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5961) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5963), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5963) });
+
+            migrationBuilder.UpdateData(
+                table: "Region",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedOnUTC", "UpdatedOnUTC" },
+                values: new object[] { new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5965), new DateTime(2021, 12, 3, 15, 19, 3, 573, DateTimeKind.Utc).AddTicks(5965) });
+        }
+    }
+}
